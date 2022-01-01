687c05cfbf076f160acb44c61693bf819e5114dd
fawtytoo <fawtytoo@gmx.co.uk>
2022-01-01 12:20:58 +0000

    Animate icons on hover
    Hovering on icons increases their point size to 18pt to give an animation.

--- extension.js
+++ extension.js
@@ -4,10 +4,18 @@ const St = imports.gi.St;
 const Clutter = imports.gi.Clutter;
 const Meta = imports.gi.Meta;
 const Lang = imports.lang;
+const Config = imports.misc.config;
+const Version = parseInt(Config.PACKAGE_VERSION.split('.')[0]);
+
+const ShowAppsButton = Version == 3 ? Main.overview.viewSelector._showAppsButton : Main.overview.dash.showAppsButton;
+const MainOverview = Version == 3 ? Main.overview.viewSelector : Main.overview.dash;
+const OverviewShowApps = Version == 3 ? Main.overview.viewSelector : Main.overview;
+
+const DirBack = Version == 3 ? Meta.MotionDirection.UP : Meta.MotionDirection.LEFT;
+const DirForward = Version == 3 ? Meta.MotionDirection.DOWN : Meta.MotionDirection.RIGHT;
 
 let activities;
 let button;
-
 
 const ActivitiesIcons = new Lang.Class({
     Name: 'ActivitiesIcons',
@@ -22,17 +30,17 @@ const ActivitiesIcons = new Lang.Class({
         this.box = new St.BoxLayout({style_class: 'activity-box'});
 
         this.appButton = new St.Button();
-        this.appIcon = new St.Icon({icon_name: 'view-app-grid-symbolic', style_class: 'activity-icon'});
-        this.appButton.add_actor(this.appIcon);
+        this.appButton.child = new St.Icon({reactive: true, track_hover: true, icon_name: 'view-app-grid-symbolic', style_class: 'system-status-icon activity-icon'});
         this.appButton.connect('clicked', () => this._changePage(true));
         this.appButton.connect('scroll-event', (actor, event) => this._scrollWindows(actor, event));
+        this.appButton.child.connect("notify::hover", (widget) => {widget.style_class = (widget.hover) ? "activity-icon-hover" : "activity-icon"});
         this.box.add_actor(this.appButton);
 
         this.overButton = new St.Button();
-        this.overIcon = new St.Icon({icon_name: 'focus-windows-symbolic', style_class: 'activity-icon'});
-        this.overButton.add_actor(this.overIcon);
+        this.overButton.child = new St.Icon({reactive: true, track_hover: true, icon_name: 'focus-windows-symbolic', style_class: 'system-status-icon activity-icon'});
         this.overButton.connect('clicked', () => this._changePage(false));
         this.overButton.connect('scroll-event', (actor, event) => this._scrollWorkspace(actor, event));
+        this.overButton.child.connect("notify::hover", (widget) => {widget.style_class = (widget.hover) ? "activity-icon-hover" : "activity-icon"});
         this.box.add_actor(this.overButton);
 
         this.actor.add_child(this.box);
@@ -46,18 +54,26 @@ const ActivitiesIcons = new Lang.Class({
     _changePage(appsButtonChecked)
     {
         // selecting the same view again will hide the overview
-        if (Main.overview.visible && appsButtonChecked == Main.overview.viewSelector._showAppsButton.checked)
+        if (Main.overview.visible && appsButtonChecked == ShowAppsButton.checked)
         {
             Main.overview.hide();
             return;
         }
 
-        Main.overview.viewSelector._showAppsButton.checked = appsButtonChecked;
+        ShowAppsButton.checked = appsButtonChecked;
 
         if (!Main.overview.visible)
-            Main.overview.show();
+        {
+            if (appsButtonChecked)
+                OverviewShowApps.showApps();
+            else
+                Main.overview.show();
+        }
         else
-            Main.overview.viewSelector._showAppsButtonToggled();
+        {
+            ShowAppsButton.checked = appsButtonChecked;
+            MainOverview._onShowAppsButtonToggled();
+        }
     },
 
     _scrollWindows(actor, event)
@@ -91,13 +107,13 @@ const ActivitiesIcons = new Lang.Class({
             if (workspace.index() == 0)
                 return;
             else
-                workspace.get_neighbor(Meta.MotionDirection.UP).activate(global.get_current_time());
+                workspace.get_neighbor(DirBack).activate(global.get_current_time());
             break;
           case Clutter.ScrollDirection.DOWN:
             if (workspace.index() + 1 == this.wm.n_workspaces)
                 return;
             else
-                workspace.get_neighbor(Meta.MotionDirection.DOWN).activate(global.get_current_time());
+                workspace.get_neighbor(DirForward).activate(global.get_current_time());
             break;
         }
 
